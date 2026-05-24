ee0feca6c14cc7749e0377a24725b0f09d165287
Justin Sorrells <justinwsorrells@gmail.com>
2026-05-24 15:50:13 -0600

    just pushing the latest

--- server/server.py
+++ server/server.py
@@ -1,28 +1,67 @@
+import inspect
+import json
 import socket
 
-def hello(name):
-    print(f'Hello, {name}')
-    return f'Hello, {name}'
+DEBUG = 1
 
-def parse_cmd(cmd):
-    return cmd.split(',')
+
+def hello(name: str) -> str:
+    print(f"Hello, {name}")
+    return f"Hello, {name}"
+
+
+def parse_data(packet):
+    obj = json.loads(packet)
+    return obj
+
+
+schema = {
+    "info": {
+        "function": None,
+        "args": [],
+    },
+    "hello": {
+        "function": hello,  # type: ignore[syntax]
+        "args": ["str"],
+    },
+}
+
+#timestamps
+#sequence_number
+#status_code
+#cmd_output
+#error
+
+def evaluate_command(cmd, args):
+    if cmd not in schema:
+        raise ValueError(f"{cmd} no in schema")
+    func = schema[cmd]["function"]
+    sig = inspect.signature(cmd)
+    try:
+        sig.bind(*args)
+    except TypeError as e:
+        raise ValueError(f"bad arguments for {cmd}: {e}")
+    return func(*args)
+
+def generate_response(packet):
+    pass 
+
+def info():
+    return json.dumps(schema)
+
 
 if __name__ == "__main__":
-    available = {
-        "hello": {
-            "function"=hello,
-            "args"=["str"],
-        },        
-    }
+    schema["info"]["function"] = info  # inject info command into schema
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serversocket.bind(('0.0.0.0', 6767))
-    print(serversocket)
+    serversocket.bind(("0.0.0.0", 6767))
+    if DEBUG == 1:
+        print(serversocket)
     while True:
         data, addr = serversocket.recvfrom(1024)
-        parsed_cmd = data.decode('utf-8').strip().lower().split(',')
-        cmd = parsed_cmd[0]
-        if cmd in available:
-            res = available[cmd]().encode('utf-8')
-            serversocket.sendto(res, addr)
-        print(cmd)
+        packet = parse_data(data)
+        if packet["type"] == "command":
+            response = generate_response(packet)
+            serversocket.sendto(response, addr)
+
+
