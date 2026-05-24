95bb6edd45a440535d2a3c66bf126c65a9b1c130
Justin Sorrells <justinwsorrells@gmail.com>
2026-05-23 19:32:53 -0600

    adding melanie's client code

---
+++ client/client.py
@@ -0,0 +1,14 @@
+import socket
+import time
+
+HOST = 'put hostname here'
+PORT = 6767
+
+if __name__ == "__main__":
+    while True:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print("sending hello")
+        clientSocket.sendto(b"hello", (HOST, PORT))
+        dataRec = clientSocket.recvfrom(1024)
+        print("data recieved: ", dataRec)
+        time.sleep(1)
